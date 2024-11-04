d5da6747eed1ea29d8b83a3344bf6a63d8012098
pedrohassen <pedro_hasse@yahoo.com.br>
2024-11-04 12:04:34 -0300

    ✨ (Main.jsx): add ModalForm component to Main component for improved user interaction and functionality

--- src/components/Main.jsx
+++ src/components/Main.jsx
@@ -1,13 +1,18 @@
+import { ModalForm } from "./ModalForm";
+
 export function Main() {
   return (
-    <main>
-      <div className='register__btn'>
-        <h2>Cadastro de Items CircuitHub</h2>
-        <button>Adicionar</button>
-      </div>
-      <div>
-        <p>Placeholder: aqui vai a lista dos items</p>
-      </div>
-    </main>
+    <>
+      <main>
+        <div className='register__btn'>
+          <h2>Cadastro de Items CircuitHub</h2>
+          <button>Adicionar</button>
+        </div>
+        <div>
+          <p>Placeholder: aqui vai a lista dos items</p>
+        </div>
+      </main>
+      <ModalForm />
+    </>
   )
 }
