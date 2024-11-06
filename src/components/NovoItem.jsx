5a8c5bb30a25de3612aab91ccd1523146fc230f8
Filipe Silveira Maciel <filipe10sm@gmail.com>
2024-11-06 14:30:23 -0300

    Componente NovoItem Atualizado

--- src/components/NovoItem.jsx
+++ src/components/NovoItem.jsx
@@ -4,7 +4,7 @@ export function NovoItem({item, setItem}){
 
     const {register, handleSubmit, reset} = useForm()
 
-    function incluirFilme(data) {
+    function incluirItem(data) {
         const novo = {
             nome: data.nome,
             categoria: data.categoria,
@@ -15,8 +15,50 @@ export function NovoItem({item, setItem}){
             comentario: ""
 
         }
+        const item2 = [novo, ...item]
+        setItem(item2)
+        localStorage.setItem("item", JSON.stringify(item2))
     }
 
+    return(
+        <>
+        <h2>Cadastro de itens</h2>
+
+        <form onSubmit={handleSubmit(incluirItem)}>
+
+        <p>
+            <label htmlFor="nome">Produto: </label>
+            <input type="text" name="" id="nome" required {...register('nome')}/>
+        </p>
+
+        <p>
+            <label htmlFor="categoria">Categoria: </label>
+            <input type="text" required {...register('categoria')}/>
+        </p>
+
+        <p>
+            <label htmlFor="marca">Marca: </label>
+            <input type="text" required {...register('marca')}/>
+        </p>
+
+        <p>
+            <label htmlFor="preco">Preço: </label>
+            <input type="number" required {...register('preco')}/>
+        </p>
+
+        <p>
+            <label htmlFor="foto">Foto: </label>
+            <input type="text" required {...register('foto')}/>
+        </p>
+
+        <p>
+            <input type="submit" value="Incluir"/>
+            <input type="reset" value="Limpar" />
+        </p>
+
+        </form>
+        </>
+    )
 
 
 
