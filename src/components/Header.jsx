3a919b2d88e06d528fc9ee5ee6e8043e2ec8c6e8
Thiago Vieira <53668902+Cabreira97@users.noreply.github.com>
2024-11-05 21:35:18 -0300

    Modifica Header

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,7 +1,23 @@
+import { Link } from "react-router-dom";
+
 export function Header() {
   return (
     <header>
-      <h1>Header</h1>
+      <div className="div-titulo">
+        <img src="" alt="CircuitHub Logo" />
+        <div>
+          <h1>CircuitHub</h1>
+          <h2>Seu Hub de Hardware e Gadjets</h2>
+        </div>
+      </div>
+      <div className="div-links">
+        <p>
+          <Link to='/' className="links">Cadastro</Link>
+        </p>
+        <p>
+          <Link to='/pesquisa' className="links">Pesquisa</Link>
+        </p>
+      </div>
     </header>
   )
 }
