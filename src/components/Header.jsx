c32091930dd8e4485d53e432320c56ddfff6a629
unknown <pedro_hasse@yahoo.com.br>
2024-11-08 20:04:43 -0300

    Na página Pesquisas, o Header não tem mais o vídeo CircuitHub

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -29,13 +29,19 @@ export function Header() {
           </p>
         </div>
       </section>
+      {location.pathname === '/pesquisa' ? ''
+      :
+      <>
         <video className="absolute inset-0 z-[-4] w-[100vw]" autoPlay loop muted playsInline>
           <source src="1108.mp4" type="video/mp4" />
         </video>
-      <section className="flex justify-center flex-col items-center font-roboto mt-52 mb-[40vh] bg-gray-900 p-8">
-        <h1 className=" text-[10rem] text-white">CircuitHub</h1>
-        <h2 className="text-[2rem] text-neutral-300">Seu Hub de Hardware e Gadjets</h2>
-      </section>
+        <section className="flex justify-center flex-col items-center font-roboto mt-52 mb-[40vh] bg-gray-900 p-8">
+          <h1 className=" text-[10rem] text-white">CircuitHub</h1>
+          <h2 className="text-[2rem] text-neutral-300">Seu Hub de Hardware e Gadjets</h2>
+        </section>
+      </>
+      }
+
     </header>
   );
 }
